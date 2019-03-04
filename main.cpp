2eb0af3a537f0e9b8e7ee8e61716ad3316f8693a
Artoghrul <ertogrulgehremanli@gmail.com>
2019-03-04 13:38:56 +0300

    idk

--- main.cpp
+++ main.cpp
@@ -1,43 +1,74 @@
 #include <iostream>
 #include <cstring>
+#include "sqlite3.h"
 #include "entry.h"
-#include <sqlite3.h>
 
 using namespace std;
 void add(Entry *entries, string entry, int n);
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << "\n";
+    }
+    cout << "\n";
+    return 0;
+}
 int main(int argc, char *argv[])
 {
-    Entry *entries;
-    entries = new Entry[255];
-    int n; //number of entries
-    fstream input;
-    //checkOptions
-    input.open("entries.txt");
-    input >> n;
-    if (!strcmp(argv[1], "add"))
+    const int STATEMENTS = 6;
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    const char *pSQL[STATEMENTS];
+    int rc;
+
+    rc = sqlite3_open("artodo.db", &db);
+
+    if (rc)
     {
-        cout << "slkdjf";
-        entries[n].setDetails(argv[2]);
-        input << argv[2] << endl;
+        cout << "Can't open database: " << sqlite3_errmsg(db) << "\n";
+    }
+    else
+    {
+        cout << "Open database successfully\n\n";
+    }
+
+    pSQL[0] = "create table if not exists myTable (FirstName varchar(30), LastName varchar(30), Age smallint, Hometown varchar(30), Job varchar(30))";
+
+    pSQL[1] = "insert into myTable (FirstName, LastName, Age, Hometown, Job) values ('Peter', 'Griffin', 41, 'Quahog', 'Brewery')";
+
+    pSQL[2] = "insert into myTable (FirstName, LastName, Age, Hometown, Job) values ('Lois', 'Griffin', 40, 'Newport', 'Piano Teacher')";
+
+    pSQL[3] = "insert into myTable (FirstName, LastName, Age, Hometown, Job) values ('Joseph', 'Swanson', 39, 'Quahog', 'Police Officer')";
+
+    pSQL[4] = "insert into myTable (FirstName, LastName, Age, Hometown, Job) values ('Glenn', 'Quagmire', 41, 'Quahog', 'Pilot')";
+
+    pSQL[5] = "select * from myTable";
+
+    //pSQL[6] = "delete from myTable";
+    //pSQL[7] = "drop table myTable";
+
+    for (int i = 0; i < STATEMENTS; i++)
+    {
+        cout << i << endl;
+        rc = sqlite3_exec(db, pSQL[i], callback, 0, &zErrMsg);
+        if (rc != SQLITE_OK)
+        {
+            cout << "SQL error: " << sqlite3_errmsg(db) << "\n";
+            sqlite3_free(zErrMsg);
+            break;
         }
-    else if (argv[1] == "rm")
-    {
-        // remove(argv[2]);
-    }
-    else if (argv[1] == "list")
-    {
-        //list();
     }
 
-    cout << ("Hello! Showing last 5 entries:") << endl;
+    sqlite3_close(db);
 
-    for (int i = 0; i < 5; i++)
-    {
-        entries[i].print();
-    }
+    return 0;
 }
 
 void add(Entry *entries, string input, int n)
 {
+    cout << "slkdjf";
+    entries[n].setDetails(input);
 }
